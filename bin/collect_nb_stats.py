58a7411d8f86111c615c0f967a999db0a53cf676
Sergey Abramov <aswq22013@gmail.com>
2022-05-24 02:50:32 +0300

    debug

--- bin/collect_nb_stats.py
+++ bin/collect_nb_stats.py
@@ -4,13 +4,19 @@ import pandas as pd
 import os
 
 def main(badmaps, fit_dir):
-    concat_pval_dfs = pd.read_table(badmaps)
+    dfs = []
+    with open(badmaps) as f:
+        for line in f:
+            dfs.append(pd.read_table(line.strip()))
+    concat_pval_dfs = pd.concat(dfs)
     for BAD in concat_pval_dfs['BAD'].unique():
+        bad_dir = os.path.join(fit_dir, 'BAD{:.2f}'.format(BAD))
+        if not os.path.exists(bad_dir):
+            os.mkdir(bad_dir)
         concat_pval_dfs[concat_pval_dfs['BAD'] == BAD].groupby(['ref_counts', 'alt_counts']).size().reset_index(name='counts').to_csv(
-            os.path.join(fit_dir, 'BAD{:.2f}'.format(BAD), 'stats.tsv'), sep='\t', index=False, header=None
+            os.path.join(bad_dir, 'stats.tsv'), sep='\t', index=False, header=None
         )
-
-
+        
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Collect NB stats')
     parser.add_argument('-O', help='Dir to save calculated p-value into')
