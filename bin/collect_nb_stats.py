bf534c3478bed1280efa99e8445a04d01bf3fab6
Sergey Abramov <aswq22013@gmail.com>
2022-05-24 02:20:08 +0300

    debug

--- bin/collect_nb_stats.py
+++ bin/collect_nb_stats.py
@@ -4,11 +4,7 @@ import pandas as pd
 import os
 
 def main(badmaps, fit_dir):
-    dfs = []
-    with open(badmaps) as f:
-        for line in f:
-            dfs.append(pd.read_table(line.strip()))
-    concat_pval_dfs = pd.concat(dfs)
+    concat_pval_dfs = pd.read_table(badmaps)
     for BAD in concat_pval_dfs['BAD'].unique():
         concat_pval_dfs[concat_pval_dfs['BAD'] == BAD].groupby(['ref_counts', 'alt_counts']).size().reset_index(name='counts').to_csv(
             os.path.join(fit_dir, 'BAD{:.2f}'.format(BAD), 'stats.tsv'), sep='\t', index=False, header=None
