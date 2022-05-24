32b081bc9111d8de80f0bb6bdd978161d7e7a3b9
Sergey Abramov <aswq22013@gmail.com>
2022-05-24 14:52:42 +0300

    join to merge

--- bin/filter_cavs.py
+++ bin/filter_cavs.py
@@ -9,7 +9,7 @@ def main(agg_file, snps_file, output_file, fdr_tr):
         df = df[df[[f'fdrp_bh_{allele}' for allele in alleles]].min(axis=1) >= fdr_tr]
     snps_df = pd.read_table(snps_file)
     if not snps_df.empty:
-        snps_df = snps_df.join(df, on=join_columns, how='inner')[join_columns + ['ref_counts', 'alt_counts']]
+        snps_df = snps_df.merge(df, on=join_columns, how='inner')[join_columns + ['ref_counts', 'alt_counts']]
 
     snps_df.to_csv(output_file, sep='\t', index=False)
 
