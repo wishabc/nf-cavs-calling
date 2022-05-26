ced6a4452078fc18d952632eac7be9827021da0e
Sergey Abramov <aswq22013@gmail.com>
2022-05-26 03:47:59 +0300

    debug

--- bin/stats_to_df.py
+++ bin/stats_to_df.py
@@ -6,15 +6,13 @@ import sys
 
 nb_as_params = []
 for file in sys.argv[3:]:
-    print(file)
-for allele in alleles:
-    for bad in check_states(sys.argv[1]):
-        df = pd.read_table(os.path.join(sys.argv[1],
-         'BAD{:.2f}'.format(bad),
-          'NBweights_{}.tsv'.format(allele)))
-        df['allele'] = allele
-        df['BAD'] = bad
-        nb_as_params.append(df)
+    parsed_name, bad, ext = file.split('.')
+    allele = parsed_name[-3:]
+    bad = float(bad)
+    df = pd.read_table(os.path.join(file))
+    df['allele'] = allele
+    df['BAD'] = bad
+    nb_as_params.append(df)
 nb_as_params = pd.concat(nb_as_params).reset_index().rename(columns={'index': 'fix_c'})[
     ['allele', 'fix_c', 'BAD', 'r', 'w']
-].to_csv(sys.argv[2], sep='\t', index=False)
+].to_csv(sys.argv[1], sep='\t', index=False)
