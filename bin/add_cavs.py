cda86ca698947820f3b8c6f6c4a580f7d83af188
Sergey Abramov <aswq22013@gmail.com>
2022-05-28 02:56:52 +0300

    debug

--- bin/add_cavs.py
+++ bin/add_cavs.py
@@ -1,17 +1,21 @@
 import pandas as pd
 import argparse
 
+def add_key(df):
+    df['key'] = df['#chr'] + '@' + df['start']
+    return df
+
 def main(new_badmap, old_badmap, output):
-    new_df = pd.read_table(new_badmap)
-    old_df = pd.read_table(old_badmap)
-    rs_ids = new_df['ID'].unique()
-    print([x for x in rs_ids if not x.startswith('rs')])
-    imputed_cavs = old_df[~old_df['ID'].isin(rs_ids)]
+    new_df = add_key(pd.read_table(new_badmap))
+    old_df = add_key(pd.read_table(old_badmap))
+    rs_ids = new_df['key'].unique()
+    
+    imputed_cavs = old_df[~old_df['key'].isin(rs_ids)]
     df = pd.concat([new_df, imputed_cavs])
     if len(df.index) != len(old_df.index):
         print(len(df.index), len(old_df.index), len(rs_ids), len(imputed_cavs.index))
         raise AssertionError
-    df.to_csv(output, sep='\t', index=False)
+    df[[x for x in df.columns if x != 'key']].to_csv(output, sep='\t', index=False)
 
 
 if __name__ == '__main__':
