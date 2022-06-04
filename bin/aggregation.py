af7c0fa8ac78abb3b3113db048c1405b856abc79
Sergey Abramov <aswq22013@gmail.com>
2022-06-05 00:58:44 +0300

    debug

--- bin/aggregation.py
+++ bin/aggregation.py
@@ -24,8 +24,9 @@ def aggregate_snp(snp_df):
 
 
 def aggregate_es(es_array, p_array):
-    es, p = zip(*[(x, y) for x, y in zip(es_array, p_array) if y != 1 and not pd.isna(y)])
-    if len(es) > 0:
+    res = zip(*[(x, y) for x, y in zip(es_array, p_array) if y != 1 and not pd.isna(y)])
+    if len(res) > 0:
+        es, p = res
         weights = [-1 * np.log10(x) for x in p]
         es_mean = np.round(np.average(es, weights=weights), 3)
     else:
