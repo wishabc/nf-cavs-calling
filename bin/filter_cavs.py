d260bb1dfa338c8afc510385bae68032f0473253
Sergey Abramov <aswq22013@gmail.com>
2022-06-15 04:15:57 +0300

    fixed cavs filter

--- bin/filter_cavs.py
+++ bin/filter_cavs.py
@@ -6,7 +6,8 @@ def main(agg_file, snps_file, output_file, fdr_tr):
     df = pd.read_table(agg_file)
     join_columns = ['#chr', 'start', 'end', 'ID', 'ref', 'alt']
     if not df.empty:
-        df = df[df[[f'fdrp_bh_{allele}' for allele in alleles]].min(axis=1) >= fdr_tr]
+        df['min_fdr'] = df[[f'fdrp_bh_{allele}' for allele in alleles]].min(axis=1)
+        df = df[(df['min_fdr'] >= fdr_tr) | pd.isna(df['min_fdr'])]
     snps_df = pd.read_table(snps_file)
     if not snps_df.empty:
         snps_df = snps_df.merge(df, on=join_columns, how='inner')[join_columns + ['ref_counts', 'alt_counts']]
