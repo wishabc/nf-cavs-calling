3d7225c20649f3604b1918efc035a3f2ee867343
wishabc <aswq22013@gmail.com>
2022-12-04 22:19:58 -0800

    extract files

---
+++ extract_files_by_symlinks.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# Usage: extract symlink <target-dir> <-f | -n>
+# -f moves symlink targets
+# -n perform a 'dry run', printing all the commands to be performed
+
+
+function extract_symlink () {
+	if ! [ -d "$file" ]; then
+        symlink_target=$( readlink "$file" )
+        if [[ "$symlink_target" != "$file" ]]; then
+            mv $symlink_target $1
+        fi
+	fi
+}
+
+case $2 in
+    "-n")
+        find $1 -exec echo "moving {}" \;
+        ;;
+    "-f")
+        find $1 -exec extract_symlink {} \;
+        ;;
+    "*") 
+        echo "Neither -f nor -n are provided"
+esac
