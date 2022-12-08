6f093d6725bd26fa41296104aca19767d637dbdb
wishabc <aswq22013@gmail.com>
2022-12-08 13:58:57 -0800

    added tqdm for debug

--- bin/annotate_with_phenotypes.py
+++ bin/annotate_with_phenotypes.py
@@ -166,6 +166,7 @@ def arr_to_str(arr):
 def get_phens_by_id(row, all_phenotypes, ids_phenotypes_dict, gtex):
     snp_id = row['ID']
     snp_posid = row.posID
+    print(ids_phenotypes_dict)
     return [arr_to_str([ids_phenotypes_dict[y]
                                 for y in all_phenotypes.get(snp_id, {}).get(x) if y is not None])
                                 for x in phenotype_db_names] + [arr_to_str(snps_dict.get(snp_posid, [None, None])[1]) for snps_dict in gtex.values()]
