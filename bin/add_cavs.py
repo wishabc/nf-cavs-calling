4f8c6285220ec829e6215c2f7ddf62e79fceffb0
wishabc <aswq22013@gmail.com>
2022-12-26 20:46:05 -0800

    fix

--- bin/add_cavs.py
+++ bin/add_cavs.py
@@ -14,7 +14,7 @@ def main(new_badmap, old_badmap, output):
         new_df.to_csv(output, sep='\t', index=False)
         return
     old_df = set_index(pd.read_table(old_badmap))
-    
+    print(new_df.index)
     imputed_cavs = old_df[~new_df.index]
     df = pd.concat([new_df, imputed_cavs])
     if len(df.index) != len(old_df.index):
