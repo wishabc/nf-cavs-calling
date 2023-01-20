calculation of pvalue and es fixed

bin/aggregation.py
@@ -1,26 +1,29 @@
 import argparse
 import pandas as pd
-from helpers import get_field_by_ftype, alleles, starting_columns
+from helpers import alleles, starting_columns
 from scipy.stats import combine_pvalues
 from statsmodels.stats.multitest import multipletests
 import numpy as np
 import multiprocessing as mp
+import math
 
-pval_file_leave_columns = [*starting_columns, 'AAF', 'RAF', 'FMR']
-result_columns = pval_file_leave_columns + [ 'mean_BAD', 
+
+keep_columns = [*starting_columns, 'AAF', 'RAF', 'FMR']
+result_columns = keep_columns + [ 'mean_BAD', 
     '# of SNPs', 'max_cover', 'footprints_n',
     'es_weighted_mean', 'es_mean', 
-    'logitp_ref', 'logitp_alt'
+    'logit_pval_ref', 'logit_pval_alt'
     ]
 
 def aggregate_snp(snp_df):
     pvals = {}
     effect_sizes = {}
     for allele in alleles:
-        pvals[allele] = logit_aggregate_pvalues(snp_df[get_field_by_ftype(allele)])
-        effect_sizes[allele] = aggregate_es(snp_df[get_field_by_ftype(allele, 'es')],
-                                            snp_df[[get_field_by_ftype(al) for al in alleles]].min(axis=1),
-                                            snp_df['coverage'])
+        pvals[allele] = logit_aggregate_pvalues(snp_df[f'pval_{allele}'])
+
+    effect_sizes = aggregate_es(snp_df['es'],
+                                snp_df[['pval_ref', 'pval_alt']].min(axis=1),
+                                snp_df['coverage'])
     mean_BAD = snp_df['BAD'].mean()
 
     footprints_n = 0
@@ -60,17 +63,18 @@ def logit_aggregate_pvalues(pval_list):
     return combine_pvalues(pvalues, method='mudholkar_george')[1]
 
 def df_to_group(df):
-    return df.groupby(pval_file_leave_columns , as_index=False)
+    return df.groupby(starting_columns, as_index=False)
 
 def aggregate_apply(df):
-    new_df = df.loc[:, pval_file_leave_columns].head(1)
+    new_df = df.loc[:, keep_columns].head(1)
     mean_BAD, pvals, effect_sizes, footprints_n = aggregate_snp(df)
+    es_mean, es_weighted_mean = effect_sizes
     new_df['mean_BAD'] = mean_BAD
     new_df['footprints_n'] = footprints_n
     for allele in alleles:
-        new_df[get_field_by_ftype(allele, 'pval-ag')] = pvals[allele]
-    new_df['es_mean'] = effect_sizes['ref'][0]
-    new_df['es_weighted_mean'] = effect_sizes['ref'][1]
+        new_df[f'logit_pval_{allele}'] = pvals[allele]
+    new_df['es_mean'] = es_mean
+    new_df['es_weighted_mean'] = es_weighted_mean
     new_df['# of SNPs'] = len(df.index)
     new_df['max_cover'] = df.eval('coverage').max()
     return new_df
@@ -94,9 +98,7 @@ def aggregate_pvalues_df(pval_df_path, jobs, cover_tr):
     groups_list = list(groups.groups)
     j = min(jobs,
         max(1, mp.cpu_count()))
-    n = len(groups_list) // j
-    if n == 0:
-        n = 1
+    n = math.ceil(len(groups_list) / j)
     subgroups = [[groups.get_group(x) for x in groups_list[i: i+n]] for i in range(0, len(groups_list), n)]
     ctx = mp.get_context('forkserver')
 
@@ -113,9 +115,10 @@ def calc_fdr(aggr_df):
             fdr_arr = None
         else:
             _, fdr_arr, _, _ = multipletests(
-                aggr_df[get_field_by_ftype(allele, 'pval-ag')],
+                aggr_df[f'logit_pval_{allele}'],
                 alpha=0.05,
-                    method='fdr_bh')
+                method='fdr_bh'
+            )
         aggr_df[f"fdrp_bh_{allele}"] = fdr_arr
     aggr_df['min_fdr'] = aggr_df[[f'fdrp_bh_{x}' for x in alleles]].min(axis=1)
     return aggr_df

bin/calc_pval_binom.py
@@ -1,6 +1,6 @@
 import pandas as pd
 from scipy.stats import binom, nbinom
-from helpers import alleles, get_field_by_ftype
+from helpers import alleles
 import argparse
 import numpy as np
 
@@ -79,7 +79,10 @@ def modify_w_binom(n, p, ws, counts):
 
 # Both negbin and binom
 def odds_es(x, n, BAD, w):
-    return np.log2(x) - np.log2(n - x) + (1 - 2 * w) * np.log2(BAD) # w * np.log2(OR / BAD) + (1 - w) * np.log2(OR * BAD)
+    return np.log2(x) - np.log2(n - x) + (1 - 2 * w) * np.log2(BAD) 
+    
+    # w * np.log2(OR / BAD) + (1 - w) * np.log2(OR * BAD)
+
 
 def recalc_ws(ws, p1, p2):
     idx = (ws != 1) & (ws != 0)
@@ -88,66 +91,37 @@ def recalc_ws(ws, p1, p2):
 
 
 
-def calc_pval_for_indiv(input_filename, output_filename, stats_file=None, mode='binom', allele_tr=5, modify_w=False, es_method='exp'):
+def calc_pval_for_indiv(input_filename, output_filename, allele_tr=5, modify_w=False, es_method='exp'):
     df = pd.read_table(input_filename)
-    if mode == 'negbin' and stats_file is not None:
-        stats_df = pd.read_table(stats_file)
-    else:
-        stats_df = None
-    df = calc_pval_for_df(df, stats_df, mode, allele_tr,modify_w=modify_w, es_method=es_method)
+    df = calc_pval_for_df(df, allele_tr,modify_w=modify_w, es_method=es_method)
     df.to_csv(output_filename, sep='\t', index=None)
 
 
-def calc_pval_for_df(df, nb_params, mode, allele_tr, modify_w, es_method):
+def calc_pval_for_df(df, allele_tr, modify_w, es_method):
     if df.empty:
-        for allele in alleles:
-            for ftype in result_ftypes:
-                df[get_field_by_ftype(allele, ftype=ftype)] = None
+        cols = ['w', 'es', 'pval_ref', 'pval_alt']
+        for col in cols:  
+            df[col] = np.nan
         return df
     p = df.eval('1 / (BAD + 1)').to_numpy()
     n = df.eval('alt_counts + ref_counts').to_numpy()
+    ref_counts = df['ref_counts'].to_numpy()
     BADS = df['BAD'].to_numpy()
-    for allele in alleles:
-        counts = df[f'{allele}_counts'].to_numpy()
-        if mode == 'binom':
-            ws = np.full(counts.shape, 0.5, dtype=np.float128)
-            if modify_w:
-                ws = modify_w_binom(n, p, ws, counts)
-            else:
-                ws = np.full(counts.shape, 1, dtype=np.float128)
-            df[get_field_by_ftype(allele, 'w')] = ws
-            df[get_field_by_ftype(allele)] = censored_binom_pvalue(counts, n, p, ws, allele_tr)
 
-            if es_method == 'exp':
-                es_list = binom_es(counts, n, p, ws)
-            elif es_method == 'odds':
-                es_list = odds_es(counts, n, BADS, ws)
-            elif es_method == 'cons':
-                es_list = odds_es(counts, n, BADS, 1)
-            df[get_field_by_ftype(allele, 'es')] = es_list
-        elif mode == 'negbin':
-            if nb_params is None:
-                raise ValueError('NB params are required for p-value calculations')
-            merged = df.reset_index(drop=True).reset_index()\
-            .merge(nb_params[nb_params.eval(f'allele == "{allele}"')],
-             left_on=[f'{alleles[allele]}_counts', 'BAD'],
-             right_on=['fix_c', 'BAD'], sort=False, how='left', validate='m:1')\
-            .sort_values('index')
-            merged.loc[merged.eval('gof > 0.05'), 'r'] = 0
-            merged.loc[merged.eval('r == 0'), 'w'] = 1
-            merged.loc[merged.eval('r == 0'), 'r'] = merged.loc[merged.eval('r == 0'), 'fix_c']
-            rs = merged['r'].to_numpy()
-            ws = merged['w'].to_numpy()
-            if modify_w:
-                ws = modify_w_nbinom(rs, p, ws, counts)
-            df[get_field_by_ftype(allele)] = censored_nbinom_pvalue(counts, rs, p, ws, allele_tr)
-            if es_method == 'exp':
-                es_list = censored_nbinom_es(counts, rs, p, ws, allele_tr)
-            elif es_method == 'odds':
-                es_list = odds_es(counts, n, BADS, ws)
-            elif es_method == 'cons':
-                es_list = odds_es(counts, n, BADS, 1)
-            df[get_field_by_ftype(allele, 'es')] = es_list
+    ws = np.full(ref_counts.shape, 0.5, dtype=np.float128)
+    if modify_w:
+        ws = modify_w_binom(n, p, ws, ref_counts)
+    df['w'] = ws
+    if es_method == 'exp':
+        es_list = binom_es(ref_counts, n, p, ws)
+    elif es_method == 'odds':
+        es_list = odds_es(ref_counts, n, BADS, ws)
+    elif es_method == 'cons':
+        es_list = odds_es(ref_counts, n, BADS, 1)
+    df['es'] = es_list
+
+    df['pval_ref'] = censored_binom_pvalue(ref_counts, n, p, ws, allele_tr)
+    df['pval_alt'] = censored_binom_pvalue(n - ref_counts, n, p, 1 - ws, allele_tr)
     return df
 
 
@@ -155,10 +129,6 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Calculate pvalue for model')
     parser.add_argument('-I', help='BABACHI annotated BED file with SNPs')
     parser.add_argument('-O', help='File to save calculated p-value into')
-    parser.add_argument('-s', help='Strategy for p-value calculation. One of "negbin", "binom"')
-    parser.add_argument('--stats-file',
-        help='Path to file with fitted negative binomial distribution. Not required for "binom" strategy',
-        default='./')
     parser.add_argument('-a', type=int, help='Allelic reads threshold', default=5)
     parser.add_argument('--es-method',
         help='Method to calculate effect size. One of "exp", "odds", "cons"',
@@ -169,8 +139,6 @@ if __name__ == '__main__':
     calc_pval_for_indiv(
         input_filename=args.I,
         output_filename=args.O,
-        mode=args.s,
-        stats_file=args.stats_file,
         allele_tr=args.a,
         es_method=args.es_method,
         modify_w=args.recalc_w