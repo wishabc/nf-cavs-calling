544874b9f2bd83a6a7fbef0236688082bc575c01
wishabc <aswq22013@gmail.com>
2023-06-15 14:10:35 +0300

    fixed dry run for extract symlinks

--- extract_files_by_symlinks.sh
+++ extract_files_by_symlinks.sh
@@ -14,7 +14,7 @@ function extract_symlink () {
 export -f extract_symlink
 case $2 in
     "-n")
-        find $1 -exec bash -c "a=$( echo {} ); b=$( readlink $a ); if [ $b != '' ]; then echo $a fi; echo mv $b $a" \;
+        find $1 -exec bash -c "a=$( echo {} ); b=$( readlink $a ); if [ $b != '' ]; then echo $a; fi; echo mv $b $a" \;
         ;;
     "-f")
         find $1 -exec bash -c 'extract_symlink "$@"' bash {} \;
