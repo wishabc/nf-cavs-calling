556064cd6159391a55c7d9eeb80618ba93f5471c
wishabc <aswq22013@gmail.com>
2023-06-21 13:19:13 -0700

    moved bad estimation to main

--- bin/merge_files.py
+++ bin/merge_files.py
@@ -8,9 +8,11 @@ with open(sys.argv[1], 'w') as outfile:
     for i, fname in enumerate(paths):
         with open(fname, 'r') as infile:
             for j, line in enumerate(infile):
+                if not line.endswith('\n'):
+                    line += '\n'
                 if i == 0 and j == 0:
                     new_line = line.replace('\n', '\tgroup_id\n')
-                elif i != 0 and j == 0:
+                elif i != 0 and j == 0: # Skip header
                     continue
                 else:
                     new_line = line.replace('\n', f'\t{group_key}\n')
