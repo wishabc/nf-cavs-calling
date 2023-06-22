d674097c4f3b2858d02e0ee5fccc69de697a20a8
wishabc <aswq22013@gmail.com>
2023-06-22 00:18:50 -0700

    merge files

--- bin/merge_files.py
+++ bin/merge_files.py
@@ -1,19 +1,14 @@
-import shutil
 import sys
+import pandas as pd
+from tqdm import tqdm
+
 
 paths = sys.argv[3:]
 group_key = sys.argv[2]
 
 with open(sys.argv[1], 'w') as outfile:
-    for i, fname in enumerate(paths):
-        with open(fname, 'r') as infile:
-            for j, line in enumerate(infile):
-                if not line.endswith('\n'):
-                    line += '\n'
-                if i == 0 and j == 0:
-                    new_line = line.replace('\n', '\tgroup_id\n')
-                elif i != 0 and j == 0: # Skip header
-                    continue
-                else:
-                    new_line = line.replace('\n', f'\t{group_key}\n')
-                outfile.write(new_line)
+    dfs = []
+    for fname in tqdm(paths):
+        dfs.append(pd.read_table(fname))
+    df = pd.concat(dfs)
+    df['group_id'] = group_key
