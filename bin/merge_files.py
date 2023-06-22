8476d3cef048d9aa3e8e1949efcb983f44aff1e8
wishabc <aswq22013@gmail.com>
2023-06-22 00:23:57 -0700

    merge files

--- bin/merge_files.py
+++ bin/merge_files.py
@@ -6,8 +6,5 @@ paths = sys.argv[3:]
 group_key = sys.argv[2]
 
 with open(sys.argv[1], 'w') as outfile:
-    dfs = []
-    for fname in paths:
-        dfs.append(pd.read_table(fname))
-    df = pd.concat(dfs)
+    df = pd.concat([pd.read_table(fname) for fname in paths])
     df['group_id'] = group_key
