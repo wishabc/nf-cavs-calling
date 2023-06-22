d1c20f1098c81c38c575d71051f7d7c19521a147
wishabc <aswq22013@gmail.com>
2023-06-22 00:31:04 -0700

    merge files

--- bin/merge_files.py
+++ bin/merge_files.py
@@ -1,10 +1,10 @@
 import sys
 import pandas as pd
-
+from tqdm import tqdm
 
 paths = sys.argv[3:]
 group_key = sys.argv[2]
 
 with open(sys.argv[1], 'w') as outfile:
-    df = pd.concat([pd.read_table(fname) for fname in paths])
+    df = pd.concat([pd.read_table(fname) for fname in tqdm(paths)])
     df['group_id'] = group_key
