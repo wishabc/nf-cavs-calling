9f19057adfe9711cccc16dada96e23be941a3356
wishabc <aswq22013@gmail.com>
2023-06-30 15:35:48 -0700

    aggregation rewritten

--- bin/aggregation.py
+++ bin/aggregation.py
@@ -3,30 +3,39 @@ import pandas as pd
 from helpers import alleles, starting_columns
 from scipy.stats import combine_pvalues
 from statsmodels.stats.multitest import multipletests
+from calc_pval_binom import calc_pval
 import numpy as np
-import multiprocessing as mp
-import math
 
 
 keep_columns = [*starting_columns, 'AAF', 'RAF']
-result_columns = keep_columns + ['mean_BAD', 
-    'nSNPs', 'max_cover', 'mean_cover',
+result_columns = keep_columns + [
+    'mean_BAD', 'nSNPs', 'max_cover', 'mean_cover',
     'footprints_n', 'hotspots_n',
     'es_weighted_mean', 'es_mean', 
-    'logit_pval_ref', 'logit_pval_alt'
+    'logit_pval_ref', 'logit_pval_alt', 'group_id'
     ]
 
+def calc_min_cover_by_BAD(BAD, es=1, pvalue_tr=0.05, allele_tr=5, cmax=1000):
+    covs = np.arange(allele_tr * 2, cmax + 1)
+    x = covs / (1 + np.power(2.0, -es) / BAD)
+    mask = allele_tr <= x <= covs - allele_tr
+    x = x[mask]
+    covs = covs[mask]
+    BADs = np.full(x.shape, BAD)
+    _, _, sigs, _ = calc_pval(covs, x, BADs, allele_tr=allele_tr, modify_w=True, smooth=True)
+    return np.min(covs[sigs < pvalue_tr])
 
-def calc_sum_if_exists(snp_df, column):
-    return sum([int(x) for x in snp_df[column].tolist() if x != '-']) if column in snp_df.columns else '-'
+
+def calc_sum_if_not_minus(df_column):
+    non_null_vals = [int(x) for x in df_column.tolist() if x != '-']
+    return sum(non_null_vals) if len(non_null_vals) > 0 else '-' 
 
 def aggregate_snp(snp_df):
     pvals = {allele: logit_aggregate_pvalues(snp_df[f'pval_{allele}']) for allele in alleles}
     effect_sizes = aggregate_es(snp_df['es'],
                                 snp_df[['pval_ref', 'pval_alt']].min(axis=1),
                                 snp_df['coverage'])
-    return snp_df['BAD'].mean(), pvals, effect_sizes, calc_sum_if_exists(snp_df, 'hotspots'), calc_sum_if_exists(snp_df, 'footprints')
-
+    return pvals, effect_sizes
 
 def expit(x):                                        
    return 1 / (1 + np.exp(-x))
@@ -34,21 +43,17 @@ def expit(x):
 def logit(x):
     return np.log(x) - np.log(1 - x)
 
-def aggregate_es(es_array, p_array, n_array):
-    res = [(x, y, n) for x, y, n in zip(es_array, p_array, n_array)
-             if y != 1 and not pd.isna(y) and y != 0]
-    if len(res) > 0:
-        es, p, n = zip(*res)
-        weights = [-1 * np.log10(x) for x in p]
-        es_weighted_mean = np.average(es, weights=weights)
+def aggregate_es(df):
+    df = df[~pd.isna(df['min_pval']) & (df['min_pval'] != 1) & (df['min_pval'] != 0)]
 
-        sigmas = expit(np.array(es))
-        es_mean = logit(np.average(sigmas, weights=n))
+    if df.empty:
+        es_mean = [np.nan]
+        es_weighted_mean = [np.nan]
     else:
-        es_mean = np.nan
-        es_weighted_mean = np.nan
-    return es_mean, es_weighted_mean
-
+        es_weighted_mean = np.average(df['es'], weights=-np.log10(df['pval']))
+        es_mean = logit(np.average(expit(np.array(df['es'])), weights=df['coverage']))
+    
+    return pd.DataFrame({'es_mean': es_mean, 'es_weighted_mean': es_weighted_mean}, index=df.name)
 
 def logit_aggregate_pvalues(pval_list):
     pvalues = np.array([pvalue for pvalue in pval_list if 1 > pvalue > 0])
@@ -59,60 +64,47 @@ def logit_aggregate_pvalues(pval_list):
     return combine_pvalues(pvalues, method='mudholkar_george')[1]
 
 def df_to_group(df):
-    return df.groupby(starting_columns, as_index=False)
-
-def aggregate_apply(df):
-    new_df = df.loc[:, keep_columns].head(1)
-    mean_BAD, pvals, effect_sizes, hotspots_n, footprints_n = aggregate_snp(df)
-    es_mean, es_weighted_mean = effect_sizes
-    new_df['mean_BAD'] = mean_BAD
-    new_df['footprints_n'] = footprints_n
-    new_df['hotspots_n'] = hotspots_n
-    for allele in alleles:
-        new_df[f'logit_pval_{allele}'] = pvals[allele]
-    new_df['es_mean'] = es_mean
-    new_df['es_weighted_mean'] = es_weighted_mean
-    new_df['nSNPs'] = len(df.index)
-    new_df['max_cover'] = df['coverage'].max()
-    new_df['mean_cover'] = df['coverage'].mean()
-    for extra_column in ('variant_id', 'group_id'):
-        if extra_column in df.columns:
-            new_df[extra_column] = df.iloc[0].loc[extra_column]
-    return new_df
+    return df.groupby(keep_columns)
 
 
-def aggregate_subgroup(subgroup):
-    return pd.concat([aggregate_apply(x) for x in subgroup])
-
-def aggregate_pvalues_df(pval_df, jobs, cover_tr):
-    pval_df['coverage'] = pval_df.eval('ref_counts + alt_counts')
-    if not pval_df.empty:
-        pval_df = pval_df[pval_df.eval(f'coverage >= {cover_tr}')]
-    if pval_df.empty:
-        for column in result_columns:
-            if column not in pval_df.columns:
-                pval_df[column] = None
-        return pval_df[result_columns]
-
+def aggregate_pvalues_df(pval_df):
+    pval_df['min_pval'] = pval_df[['pval_ref', 'pval_alt']].min(axis=1)
     groups = df_to_group(pval_df)
-    groups_list = list(groups.groups)
-    j = min(
-            jobs,
-            mp.cpu_count()
-        )
-    n = math.ceil(len(groups_list) / j)
-    subgroups = [[groups.get_group(x) for x in groups_list[i: i+n]] for i in range(0, len(groups_list), n)]
-    snps = []
-    if j > 1:
-        ctx = mp.get_context('forkserver')
-        with ctx.Pool(j) as pool:
-            results = [pool.apply_async(aggregate_subgroup, (g, )) for g in subgroups]
-            for r in results:
-                snps.append(r.get())
-    else:
-        for subgroup in subgroups:
-            snps.append(aggregate_subgroup(subgroup=subgroup))
-    return pd.concat(snps)
+    snp_stats = groups.agg(
+        {
+            'group_id': lambda x: x.iloc[0],
+            'BAD': np.mean,
+            'coverage': ["max", "mean"],
+            'footprints': calc_sum_if_not_minus,
+            'hotspots': calc_sum_if_not_minus,
+            'pval_ref': logit_aggregate_pvalues,
+            'pval_alt': logit_aggregate_pvalues
+        }
+    ).rename(
+        columns={
+            'BAD': 'mean_BAD',
+            'coverage': 'coverage',
+            'footprints': 'footrpints_n',
+            'hotspots': 'hotspots_n',
+            'pval_ref': 'logit_pval_ref',
+            'pval_alt': 'logit_pval_alt'
+        }
+    )
+    nSNPs = groups.size()
+    g = groups[['min_pval', 'es', 'coverage']].apply(
+            aggregate_es
+        ).join(
+            nSNPs
+        ).join(
+            snp_stats
+        ).reset_index()
+    g.columns = ["_".join(x) for x in g.columns.ravel()]
+    print(g.columns)
+    return g.rename(columns={
+            'coverage_max': 'max_cover',
+            'coverage_mean': 'mean_cover'
+        }
+    )
     
 def calc_fdr(aggr_df):
     for allele in alleles:
@@ -128,18 +120,37 @@ def calc_fdr(aggr_df):
     aggr_df['min_fdr'] = aggr_df[[f'fdrp_bh_{x}' for x in alleles]].min(axis=1)
     return aggr_df
 
-def main(input_path, out_path, cover_tr=10, jobs=1):
+def main(input_path, coverage_tr):
     pval_df = pd.read_table(input_path)
-    aggr_df = aggregate_pvalues_df(pval_df, jobs, cover_tr)
-    fdr_df = calc_fdr(aggr_df)
-    fdr_df.to_csv(out_path, sep='\t', index=False)
+    pval_df['coverage'] = pval_df.eval('ref_counts + alt_counts')
+    
+    if pval_df.empty:
+        for column in result_columns:
+            if column not in pval_df.columns:
+                pval_df[column] = None
+        return pval_df[result_columns]
+
+    if coverage_tr == 'auto':
+        by_BAD_coverage_tr = {x: calc_min_cover_by_BAD(x) for x in pval_df['BAD'].unique()}
+        pval_df = pval_df[pval_df['coverage'] >= pval_df['BAD'].apply(lambda x: by_BAD_coverage_tr[x])]
+    else:    
+        pval_df = pval_df[pval_df.eval(f'coverage >= {coverage_tr}')]
+
+    aggr_df = aggregate_pvalues_df(pval_df)
+    return calc_fdr(aggr_df)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Calculate pvalue for model')
     parser.add_argument('-I', help='BABACHI annotated BED file with SNPs')
     parser.add_argument('-O', help='File to save calculated p-value into')
-    parser.add_argument('--ct', type=int, help='Cover threshold for fdr', default=10)
-    parser.add_argument('--jobs', type=int, help='Number of jobs', default=1)
+    parser.add_argument('--ct', type=str, help="""Coverage threshold for individual variants to be aggregated.
+                                                Should be "auto" or positive integer""", default='auto')
     args = parser.parse_args()
-    main(args.I, args.O, args.ct, args.jobs)
+    try:
+        coverage_tr = int(args.ct) if args.ct != 'auto' else 'auto'
+    except ValueError:
+        print(f'Incorrect coverage threshold provided. {args.ct} not a positive integer or "auto"')
+        raise
+    final_df = main(args.I, coverage_tr)
+    final_df.to_csv(args.O, sep='\t', index=False)
--- bin/calc_pval_binom.py
+++ bin/calc_pval_binom.py
@@ -1,25 +1,37 @@
 import pandas as pd
 from scipy.stats import binom
+from scipy.special import betainc
 import argparse
 import numpy as np
 
 
-# binom dist
-def binom_sf(x, n, p, w):
-    return (1 - w) * binom.sf(x, n, p) + w * binom.sf(x, n, 1 - p)
-
-def binom_cdf(x, n, p, w):
-    return (1 - w) * binom.cdf(x, n, p) + w * binom.cdf(x, n, 1 - p)
-
-
-def censored_binom_pvalue(x, n, p, w, allele_tr):
-    norm_coef = 0
-    if allele_tr > 0:
-        norm_coef = binom_cdf(n - (allele_tr - 1), n, p, w) - binom_cdf(allele_tr - 1, n, p, w)
+def cdf(x, n, p, smooth):
+    if smooth:
+        return betainc(n - x, x + 1, 1 - p)
     else:
-        norm_coef = 1
-    return (binom_sf(x - 1, n, p, w) - binom_sf(n - (allele_tr - 1), n, p, w)) / norm_coef
+        return binom.cdf(x, n, p)
 
+
+def sf(x, n, p, smooth):
+    if smooth:
+        return betainc(x + 1, n - x, p)        
+    else:
+        return binom.sf(x, n, p)
+
+
+def binom_sf(x, n, p, w, smooth):
+    return (1 - w) * sf(x, n, p, smooth) + w * sf(x, n, 1 - p, smooth)
+
+def binom_cdf(x, n, p, w, smooth):
+    return (1 - w) * cdf(x, n, p, smooth) + w * cdf(x, n, 1 - p, smooth)
+
+
+def censored_binom_pvalue(x, n, p, w, allele_tr, smooth):
+    if allele_tr > 0:
+        norm_coef = binom_cdf(n - (allele_tr - 1), n, p, w, smooth) - binom_cdf(allele_tr - 1, n, p, w, smooth)
+        return (binom_sf(x - 1, n, p, w, smooth) - binom_sf(n - (allele_tr - 1), n, p, w, smooth)) / norm_coef
+    else:
+        return binom_sf(x - 1, n, p, w, smooth)
 
 def binom_es(x, n, p, w):
     return np.log2(
@@ -44,41 +56,47 @@ def recalc_ws(ws, p1, p2):
     ws[idx] = p1[idx] / (p1[idx] + p2[idx])
     return ws
 
+def calc_pval(n, k, BADs, allele_tr, modify_w, smooth=False):
+    p = 1 / (BADs + 1)
+    ws = np.full(k.shape, 0.5, dtype=np.float128)
+    if modify_w:
+        ws = modify_w_binom(n, p, ws, k)
+    
+    es = odds_es(k, n, BADs, ws)
+    pval_ref = censored_binom_pvalue(k, n, p, ws, allele_tr, smooth=smooth)
+    pval_alt = censored_binom_pvalue(n - k, n, p, 1 - ws, allele_tr, smooth=smooth)
+    return ws, es, pval_ref, pval_alt
+
+    
+def calc_pval_for_df(df, allele_tr, modify_w):
+    n = df.eval('alt_counts + ref_counts').to_numpy()
+    ref_counts = df['ref_counts'].to_numpy()
+    BADs = df['BAD'].to_numpy()
+
+    ws, es, pval_ref, pval_alt = calc_pval(
+        n, ref_counts, BADs,
+        allele_tr=allele_tr,
+        modify_w=modify_w,
+        smooth=False
+    )
+    df['w'] = ws
+    df['es'] = es
+    df['pval_ref'] = pval_ref
+    df['pval_alt'] = pval_alt
+    return df
 
 
-def calc_pval_for_indiv(input_filename, output_filename, allele_tr=5, modify_w=False, es_method='exp'):
-    df = pd.read_table(input_filename)
+def main(df, allele_tr=5, modify_w=False):
     df = df[df.eval(f'alt_counts >= {allele_tr} & ref_counts >= {allele_tr}')]
+    # Remove already present columns
     df = df[[x for x in df.columns if x not in ['w', 'es', 'pval_ref', 'pval_alt']]]
-    df = calc_pval_for_df(df, allele_tr=allele_tr, modify_w=modify_w, es_method=es_method)
-    df.to_csv(output_filename, sep='\t', index=None)
-
-
-def calc_pval_for_df(df, allele_tr, modify_w, es_method):
+    # Check if empty
     if df.empty:
         cols = ['w', 'es', 'pval_ref', 'pval_alt']
         for col in cols:  
             df[col] = np.nan
-        return df
-    p = df.eval('1 / (BAD + 1)').to_numpy()
-    n = df.eval('alt_counts + ref_counts').to_numpy()
-    ref_counts = df['ref_counts'].to_numpy()
-    BADS = df['BAD'].to_numpy()
-
-    ws = np.full(ref_counts.shape, 0.5, dtype=np.float128)
-    if modify_w:
-        ws = modify_w_binom(n, p, ws, ref_counts)
-    df['w'] = ws
-    if es_method == 'exp':
-        es_list = binom_es(ref_counts, n, p, ws)
-    elif es_method == 'odds':
-        es_list = odds_es(ref_counts, n, BADS, ws)
-    elif es_method == 'cons':
-        es_list = odds_es(ref_counts, n, BADS, 1)
-    df['es'] = es_list
-
-    df['pval_ref'] = censored_binom_pvalue(ref_counts, n, p, ws, allele_tr)
-    df['pval_alt'] = censored_binom_pvalue(n - ref_counts, n, p, 1 - ws, allele_tr)
+    else:
+        df = calc_pval_for_df(df, allele_tr=allele_tr, modify_w=modify_w)
     return df
 
 
@@ -87,16 +105,14 @@ if __name__ == '__main__':
     parser.add_argument('-I', help='BABACHI annotated BED file with SNPs')
     parser.add_argument('-O', help='File to save calculated p-value into')
     parser.add_argument('-a', type=int, help='Allelic reads threshold', default=5)
-    parser.add_argument('--es-method',
-        help='Method to calculate effect size. One of "exp", "odds", "cons"',
-        default='odds')
     parser.add_argument('--recalc-w', help='Specify to recalculate w',
         default=False, action="store_true")
     args = parser.parse_args()
-    calc_pval_for_indiv(
-        input_filename=args.I,
-        output_filename=args.O,
+
+    input_df = pd.read_table(args.I)
+    modified_df = main(
+        input_df,
         allele_tr=args.a,
-        es_method=args.es_method,
         modify_w=args.recalc_w
     )
+    modified_df.to_csv(args.O, sep='\t', index=None)
