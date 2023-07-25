7b2084983d01bf40a5add066f7b8a481762abf64
wishabc <aswq22013@gmail.com>
2023-07-25 11:22:05 -0700

    fixes in anova pipeline

--- bin/differential_cavs.py
+++ bin/differential_cavs.py
@@ -4,7 +4,7 @@ from statsmodels.stats.multitest import multipletests
 import numpy as np
 import pandas as pd
 
-def main(tested, pvals):
+def main(tested, pvals, fdr_tr=0.05):
     constitutive_df = calc_fdr(
         aggregate_pvalues_df(tested)
     ).rename(
@@ -15,13 +15,15 @@ def main(tested, pvals):
         np.exp(pvals['log_p_differential']),
         method='fdr_bh'
     )[1]
+    tested_length = len(tested.index)
     tested = tested.merge(
-        pvals[[*starting_columns, 'differential_FDR']]
+        pvals[[*starting_columns, 'group_id', 'differential_FDR']]
     )
+    assert len(tested.index) == tested_length
 
     # set default inividual fdr and find differential snps
     differential_cavs = calc_fdr(
-        tested[tested['differential_FDR'] <= 0.05].groupby(
+        tested[tested['differential_FDR'] <= fdr_tr].groupby(
             'group_id',
             as_index=False
         ).apply(
@@ -29,7 +31,7 @@ def main(tested, pvals):
         )
     ).rename(
         columns={'min_fdr': 'min_fdr_group'}
-    )[[*starting_columns, 'min_fdr_group']]
+    )[[*starting_columns, 'group_id', 'min_fdr_group']]
 
     # Group-wise aggregation
     return pvals.merge(constitutive_df).merge(differential_cavs, how='left')
@@ -47,5 +49,6 @@ if __name__ == '__main__':
     args = parser.parse_args()
     tested = pd.read_table(args.tested_variants)
     pvals = pd.read_table(args.pvals)
-    res_df = main(tested, pvals)
+    res_df = main(tested, pvals, args.fdr_tr)
+    assert len(res_df.index) == len(pvals.index)
     res_df.to_csv(args.outpath, sep='\t', index=False)
