dadf4af8ffb25f1e79e4f7d7b7843ee83f10353b
wishabc <aswq22013@gmail.com>
2023-08-02 16:29:12 -0700

    fixes with extract symlink script

--- extract_files_by_symlinks.sh
+++ extract_files_by_symlinks.sh
@@ -7,14 +7,14 @@
 function extract_symlink () {
 	if ! [ -d "$1" ]; then
         if [ -h "$1" ]; then
-            mv $( readlink "$1" ) $1
+            mv $( realpath "$1" ) $1
         fi
 	fi
 }
 export -f extract_symlink
 case $2 in
     "-n")
-        find $1 -exec bash -c 'a="$@"; b=$( readlink $a ); if [[ "$b" != "" ]]; then echo mv $b $a; fi' bash {} \;
+        find $1 -exec bash -c 'a="$@"; b=$( realpath $a ); if [[ "$b" != "" ]]; then echo mv $b $a; fi' bash {} \;
         ;;
     "-f")
         find $1 -exec bash -c 'extract_symlink "$@"' bash {} \;
