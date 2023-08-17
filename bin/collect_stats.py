7a183a10a2185c2cbd7b39275dc488806bbc09c4
wishabc <aswq22013@gmail.com>
2023-08-16 19:55:45 -0700

    fdr

--- bin/collect_stats.py
+++ bin/collect_stats.py
@@ -13,7 +13,9 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Collect CAVs statistics')
     parser.add_argument('I', help='File with aggregated pvalues')
     parser.add_argument('O', help='File to save statistics into')
-    parser.add_argument('--fdr', help='FDR threshold for variant to be considered a CAV', dtype=float, default=0.05)
+    parser.add_argument('--fdr',
+        help='FDR threshold for variant to be considered a CAV',
+        type=float, default=0.05)
     args = parser.parse_args()
     aggregated_variants = pd.read_table(args.I)
     res_df = main(aggregated_variants, args.fdr)
