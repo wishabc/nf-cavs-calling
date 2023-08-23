ff131d68c3d606f76eb5e4c92491c9556d71b873
wishabc <aswq22013@gmail.com>
2023-08-23 11:34:44 -0700

    calc pval wrapped in the class

--- bin/calc_pval_binom.py
+++ bin/calc_pval_binom.py
@@ -7,96 +7,74 @@ import numpy as np
 
 updated_columns = ['w', 'es', 'pval_ref', 'pval_alt', 'is_tested']
 
-def calc_min_cover_by_BAD(BAD, es=1, pvalue_tr=0.05, allele_tr=5, cmax=1000):
-    covs = np.arange(allele_tr * 2, cmax + 1)
-    x = covs / (1 + np.power(2.0, -es) / BAD)
-    mask = (allele_tr <= x) & (x <= covs - allele_tr)
-    x = x[mask]
-    covs = covs[mask]
-    BADs = np.full(x.shape, BAD)
-    _, _, sigs, _ = calc_pval(covs, x, BADs, allele_tr=allele_tr, modify_w=True, smooth=True)
-    return np.min(covs[sigs < pvalue_tr])
-
-
-def cdf(x, n, p, smooth):
-    if smooth:
-        return betainc(n - x, x + 1, 1 - p)
-    else:
-        return binom.cdf(x, n, p)
-
-
-def sf(x, n, p, smooth):
-    if smooth:
-        return betainc(x + 1, n - x, p)        
-    else:
-        return binom.sf(x, n, p)
-
-
-def binom_sf(x, n, p, w, smooth):
-    return (1 - w) * sf(x, n, p, smooth) + w * sf(x, n, 1 - p, smooth)
-
-def binom_cdf(x, n, p, w, smooth):
-    return (1 - w) * cdf(x, n, p, smooth) + w * cdf(x, n, 1 - p, smooth)
-
-
-def censored_binom_pvalue(x, n, p, w, allele_tr, smooth):
-    if allele_tr > 0:
-        norm_coef = binom_cdf(n - (allele_tr - 1), n, p, w, smooth) - binom_cdf(allele_tr - 1, n, p, w, smooth)
-        return (binom_sf(x - 1, n, p, w, smooth) - binom_sf(n - (allele_tr - 1), n, p, w, smooth)) / norm_coef
-    else:
-        return binom_sf(x - 1, n, p, w, smooth)
-
-def binom_es(x, n, p, w):
-    return np.log2(
-        x / (w * (n * (1 - p)) + (1 - w) * (n * p))
-    )
-
-def modify_w_binom(n, p, ws, counts):
-    p1 = ws * binom.pmf(counts, n, 1 - p)
-    p2 = (1 - ws) * binom.pmf(counts, n, p)
-    return recalc_ws(ws, p1, p2)
-
-
-# Both negbin and binom
-def odds_es(x, n, BAD, w):
-    return np.log2(x) - np.log2(n - x) + (1 - 2 * w) * np.log2(BAD) 
+class CalcImbalance:
+    def __init__(self, allele_tr, modify_w):
+        self.allele_tr = allele_tr
+        self.modify_w = modify_w
     
-    # w * np.log2(OR / BAD) + (1 - w) * np.log2(OR * BAD)
+    def calc_pval(self, n, k, BADs, smooth=False):
+        p = 1 / (BADs + 1)
+        ws = np.full(k.shape, 0.5, dtype=np.float64)
+        if self.modify_w:
+            ws = self.modify_w_binom(n, p, ws, k)
+        
+        es = self.odds_es(k, n, BADs, ws)
+        pval_ref = self.censored_binom_pvalue(k, n, p, ws, self.allele_tr, smooth=smooth)
+        pval_alt = self.censored_binom_pvalue(n - k, n, p, 1 - ws, self.allele_tr, smooth=smooth)
+        return [ws, es, pval_ref, pval_alt]
+
+    def calc_min_cover_by_BAD(self, BAD, es=1, pvalue_tr=0.05, cmax=1000):
+        covs = np.arange(self.allele_tr * 2, cmax + 1)
+        x = covs / (1 + np.power(2.0, -es) / BAD)
+        mask = (self.allele_tr <= x) & (x <= covs - self.allele_tr)
+        BADs = np.full(mask.sum(), BAD)
+        sigs = self.calc_pval(covs[mask], x[mask], BADs, smooth=True)[2]
+        return np.min(covs[mask][sigs <= pvalue_tr])
+
+    @staticmethod
+    def cdf(x, n, p, smooth):
+        if smooth:
+            return betainc(n - x, x + 1, 1 - p)
+        else:
+            return binom.cdf(x, n, p)
+
+    @staticmethod
+    def sf(x, n, p, smooth):
+        if smooth:
+            return betainc(x + 1, n - x, p)        
+        else:
+            return binom.sf(x, n, p)
 
 
-def recalc_ws(ws, p1, p2):
-    idx = (ws != 1) & (ws != 0)
-    ws[idx] = p1[idx] / (p1[idx] + p2[idx])
-    return ws
+    def binom_sf_mixture(self, x, n, p, weight, smooth):
+        return (1 - weight) * self.sf(x, n, p, smooth) + weight * self.sf(x, n, 1 - p, smooth)
 
-def calc_pval(n, k, BADs, allele_tr, modify_w, smooth=False):
-    p = 1 / (BADs + 1)
-    ws = np.full(k.shape, 0.5, dtype=np.float128)
-    if modify_w:
-        ws = modify_w_binom(n, p, ws, k)
+    def binom_cdf_mixture(self, x, n, p, w, smooth):
+        return (1 - w) * self.cdf(x, n, p, smooth) + w * self.cdf(x, n, 1 - p, smooth)
+
+
+    def censored_binom_pvalue(self, x, n, p, w, allele_tr, smooth):
+        if allele_tr > 0:
+            norm_coef = self.binom_cdf_mixture(n - (allele_tr - 1), n, p, w, smooth) - self.binom_cdf_mixture(allele_tr - 1, n, p, w, smooth)
+            return (self.binom_sf_mixture(x - 1, n, p, w, smooth) - self.binom_sf_mixture(n - (allele_tr - 1), n, p, w, smooth)) / norm_coef
+        else:
+            return self.binom_sf_mixture(x - 1, n, p, w, smooth)
+
+    @staticmethod
+    def odds_es(x, n, BAD, w):
+        # w * np.log2(OR / BAD) + (1 - w) * np.log2(OR * BAD)
+        return np.log2(x) - np.log2(n - x) + (1 - 2 * w) * np.log2(BAD) 
     
-    es = odds_es(k, n, BADs, ws)
-    pval_ref = censored_binom_pvalue(k, n, p, ws, allele_tr, smooth=smooth)
-    pval_alt = censored_binom_pvalue(n - k, n, p, 1 - ws, allele_tr, smooth=smooth)
-    return [ws, es, pval_ref, pval_alt]
+    def modify_w_binom(self, n, p, ws, counts):
+        p1 = ws * binom.pmf(counts, n, 1 - p)
+        p2 = (1 - ws) * binom.pmf(counts, n, p)
+        return self.recalc_ws(ws, p1, p2)
     
-def calc_pval_for_df(df, allele_tr, modify_w):
-    n = df.eval('alt_counts + ref_counts').to_numpy()
-    ref_counts = df['ref_counts'].to_numpy()
-    BADs = df['BAD'].to_numpy()
-
-    return df.assign(
-        **dict(zip(
-            updated_columns,
-            calc_pval(
-                n, ref_counts,
-                BADs=BADs,
-                allele_tr=allele_tr,
-                modify_w=modify_w,
-                smooth=False
-            )
-        ))
-    )
+    @staticmethod
+    def recalc_ws(ws, p1, p2):
+        idx = (ws != 1) & (ws != 0)
+        ws[idx] = p1[idx] / (p1[idx] + p2[idx])
+        return ws
 
 
 def main(df, coverage_tr='auto', allele_tr=5, modify_w=False):
@@ -108,14 +86,21 @@ def main(df, coverage_tr='auto', allele_tr=5, modify_w=False):
     if df.empty:
         return pd.DataFrame([], columns=result_columns)
 
+    imbalance_est = CalcImbalance(allele_tr=allele_tr, modify_w=modify_w)
     df['coverage'] = df.eval('ref_counts + alt_counts')
     if coverage_tr == 'auto':
-        by_BAD_coverage_tr = {x: calc_min_cover_by_BAD(x) for x in df['BAD'].unique()}
+        by_BAD_coverage_tr = {x: imbalance_est.calc_min_cover_by_BAD(x) for x in df['BAD'].unique()}
         df['is_tested'] = df['coverage'] >= df['BAD'].apply(lambda x: by_BAD_coverage_tr[x])
     else:    
         df['is_tested'] = df.eval(f'coverage >= {coverage_tr}')
 
-    return calc_pval_for_df(df, allele_tr=allele_tr, modify_w=modify_w)[result_columns]
+    result = imbalance_est.calc_pval(
+        df['coverage'].to_numpy(), 
+        df['ref_counts'].to_numpy(),
+        BADs=df['BAD'].to_numpy(),
+        smooth=False
+    )
+    return df.assign(**dict(zip(updated_columns, result)))[result_columns]
 
 
 if __name__ == '__main__':
