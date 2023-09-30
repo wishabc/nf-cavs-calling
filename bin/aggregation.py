d4a5ae61cb6926009c99707825e6f9ee5f97b51c
wishabc <aswq22013@gmail.com>
2023-09-29 18:49:20 -0700

    aggregate with custom weights weights

--- bin/aggregation.py
+++ bin/aggregation.py
@@ -56,17 +56,17 @@ def aggregate_pvalues(pval_list, method='stouffer'):
 
 def aggregate_pvals_stf(df, weights_dict):
     # weights = np.power(df['coverage'], 2)
-    weights = df[['BAD', 'coverage']].apply(lambda row: weights_dict[str(float(row['BAD']))][str(int(row['coverage']))], axis=1).to_numpy()
+    weights = np.power(df[['BAD', 'coverage']].apply(lambda row: weights_dict[str(float(row['BAD']))][str(int(row['coverage']))], axis=1).to_numpy(), 2)
     pval_ref_weighted = st.combine_pvalues(df['pval_ref'], method='stouffer', weights=weights)[1]
     pval_alt_weighted = st.combine_pvalues(df['pval_alt'], method='stouffer', weights=weights)[1]
-    pval_weighted = st.combine_pvalues(df['min_pval'].fillna(1), method='stouffer', weights=weights)[1]
+    #pval_weighted = st.combine_pvalues(df['min_pval'].fillna(1), method='stouffer', weights=weights)[1]
     
     # pval_ref_weighted2 = st.combine_pvalues(df['pval_ref'], method='stouffer', weights=weights)[1]
     # pval_alt_weighted2 = st.combine_pvalues(df['pval_alt'], method='stouffer', weights=weights)[1]
     # pval_weighted2 = st.combine_pvalues(df['min_pval'], method='stouffer', weights=weights)[1]
     return pd.Series(
-        [pval_ref_weighted, pval_alt_weighted, pval_weighted],
-        ["pval_ref_weighted", "pval_alt_weighted", "pval_weighted"]
+        [pval_ref_weighted, pval_alt_weighted],
+        ["pval_ref_weighted", "pval_alt_weighted"]
         )
     
 def aggregate_pvalues_df(pval_df, weights):
