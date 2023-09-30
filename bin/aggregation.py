846474d4e534b30b79a2228b92acc8fcee3c4dc4
wishabc <aswq22013@gmail.com>
2023-09-29 17:41:15 -0700

    aggregate with custom weights weights

--- bin/aggregation.py
+++ bin/aggregation.py
@@ -56,7 +56,7 @@ def aggregate_pvalues(pval_list, method='stouffer'):
 
 def aggregate_pvals_stf(df, weights_dict):
     # weights = np.power(df['coverage'], 2)
-    weights = df[['BAD', 'coverage']].apply(lambda row: weights_dict[str(float(row['BAD']))][str(row['coverage'])])
+    weights = df[['BAD', 'coverage']].apply(lambda row: weights_dict[str(float(row['BAD']))][str(row['coverage'])]).fillna(1).to_numpy()
     pval_ref_weighted = st.combine_pvalues(df['pval_ref'], method='stouffer', weights=weights)[1]
     pval_alt_weighted = st.combine_pvalues(df['pval_alt'], method='stouffer', weights=weights)[1]
     # # pval_weighted = st.combine_pvalues(df['min_pval'], method='stouffer', weights=weights)[1]
