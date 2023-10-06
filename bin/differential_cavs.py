fix in fdr calculation

bin/differential_cavs.py
@@ -15,11 +15,12 @@ def main(tested, pvals, max_cover_tr=15, differential_fdr_tr=0.05, differential_
     constitutive_df['min_fdr_overall'] = calc_fdr_pd(constitutive_df['min_pval'])
 
     pvals['differential_fdr'] = calc_fdr_pd(np.exp(pvals['log_p_differential']))
+    pvals['differential_es'] = pvals.eval('(F2 + 1) / (F2 + (N - n_groups + 1) / (n_groups - 1))')
+
     tested_length = len(tested.index)
     tested = tested.merge(
         pvals
     )
-    tested['differential_es'] = tested.eval('(F2 + 1) / (F2 + (N - n_groups + 1) / (n_groups - 1))')
     assert len(tested.index) == tested_length
 
     # set default inividual fdr and find differential snps