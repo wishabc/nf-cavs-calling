6a613ea1e21e1c9751786686ed5e413442928797
wishabc <aswq22013@gmail.com>
2024-01-21 22:37:45 -0800

    Add skipped_variants_count variable to track skipped variants

--- bin/fitMixedEffectModel.R
+++ bin/fitMixedEffectModel.R
@@ -29,7 +29,7 @@ vpcontrol <- lme4::lmerControl(
   check.rankX = "stop.deficient",
   check.conv.singular = lme4::.makeCC("ignore", tol = 1e-4)
 )
-skipped_variants_count <- 0
+globalenv()$skipped_variants_count <- 0
 process_group <- function(current_data, starting_columns_names, vpcontrol) {
   # Calculate weights within the group
   w <- current_data$inverse_mse / mean(current_data$inverse_mse)
