eb0b6248fd76788e05b132a20cbc1dc78efd6a78
wishabc <aswq22013@gmail.com>
2024-01-24 00:39:25 -0800

    Add print statement to debug merged_data

--- bin/fitMixedEffectModel.R
+++ bin/fitMixedEffectModel.R
@@ -69,7 +69,7 @@ process_group <- function(current_data, vpcontrol) {
         # Add group_id column based on the names of the coefficients
         coef_df$group_id <- gsub("group_id", "", rownames(coef_df))
         merged_data <- merge(current_data, coef_df, by="group_id")
-
+        print(merged_data)
         merged_data$es_diff <- merged_data$es - merged_data$group_es
         es_var <- aggregate(
             merged_data[c('es_diff', 'w')],
