aa589d21f8d581123d1fb694e9ad11f0e0935c5e
wishabc <aswq22013@gmail.com>
2024-01-24 01:12:09 -0800

    Refactor code to use data.table for efficiency

--- bin/fitMixedEffectModel.R
+++ bin/fitMixedEffectModel.R
@@ -68,7 +68,7 @@ process_group <- function(current_data, vpcontrol) {
 
         # Add group_id column based on the names of the coefficients
         coef_df$group_id <- gsub("group_id", "", rownames(coef_df))
-        print(merged_data)
+        print(coef_df)
         merged_data <- merge(current_data, coef_df, by="group_id")
         print(merged_data)
         merged_data$es_diff <- merged_data$es - merged_data$"Estimate"
