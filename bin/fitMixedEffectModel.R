722b169cd7bdc88a1a3e848dc9774839514e0770
wishabc <aswq22013@gmail.com>
2024-01-24 09:33:58 -0800

    Refactor aggregation and grouping in aggregation.py and fitMixedEffectModel.R

--- bin/fitMixedEffectModel.R
+++ bin/fitMixedEffectModel.R
@@ -155,8 +155,9 @@ snps_df <- fread(inpath)
 snps_df$indiv_id <- as.factor(snps_df$indiv_id)
 snps_df$group_id <- as.factor(snps_df$group_id)
 
+grouping_columns <- c('variant_id', colnames(snps_df)[1:6])
 # Split the data by the grouping columns
-results <- snps_df[, process_group(.SD, vpcontrol), by='variant_id']  
+results <- snps_df[, process_group(.SD, vpcontrol), by=.(grouping_columns)]  
 
 # Write the final dataframe to a file
 fwrite(results, file=outpath, sep = "\t", row.names = FALSE, quote = FALSE)
