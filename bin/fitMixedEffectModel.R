137726fefb6284338f0398bbd278feab0c4da437
wishabc <aswq22013@gmail.com>
2024-01-24 01:17:03 -0800

    Merge es_var into coef_df in fitMixedEffectModel.R

--- bin/fitMixedEffectModel.R
+++ bin/fitMixedEffectModel.R
@@ -76,11 +76,11 @@ process_group <- function(current_data, vpcontrol) {
         es_var <- merged_data[, .(es_var = weighted.var(es_diff, w),
                           samples_count = .N),
                       by = .(group_id)]
-        
+        print(es_var)
         colnames(es_var) <- c("group_id", "es_var", "samples_count")
         print(es_var)
         coef_df <- merge(coef_df, es_var, by="group_id")
-
+        print(coef_df)
         # Extract variance and standard deviation of random effect for indiv_id
         random_effect_variance_indiv_id <- VarCorr(full_model)$indiv_id[1,1]
 
