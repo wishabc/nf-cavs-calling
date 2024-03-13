bcd264f0861347161281e41051fff88062f13c30
wishabc <aswq22013@gmail.com>
2024-03-13 15:47:26 -0700

    Fix argument typo in add_annotations.py

--- bin/add_annotations.py
+++ bin/add_annotations.py
@@ -11,13 +11,13 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Add annotations to the bed file.')
     parser.add_argument('bed_file', type=str, help='Path to the bed file')
     parser.add_argument('output', type=str, help='Path to the output file')
-    parser.add_argument('--hotspots', type=str, help='Path to the in-hotspot indicator ifile')
+    parser.add_argument('--hotspots', type=str, help='Path to the in-hotspot indicator file')
     parser.add_argument('--footprints', type=str, help='Path to the in-footprint indicator file')
     args = parser.parse_args()
 
     df = pd.read_table(args.bed_file)
     initial_df_len = len(df.index)
-    hotspots = read_indicator(args.hotspots),
+    hotspots = read_indicator(args.hotspots)
     footprints = read_indicator(args.footprints)
     assert len(hotspots) == len(footprints) == initial_df_len, f"Hotspots {len(hotspots)} and footprints {len(footprints)} should have the same length as the bed file {initial_df_len}."
     df['hotspots'] = hotspots
