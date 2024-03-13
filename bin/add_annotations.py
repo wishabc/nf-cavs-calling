c75304005f8d91a296d0faa1d7a550b4ee6ce998
wishabc <aswq22013@gmail.com>
2024-03-13 16:06:55 -0700

    Add output file generation to add_annotations.py

--- bin/add_annotations.py
+++ bin/add_annotations.py
@@ -27,4 +27,5 @@ if __name__ == '__main__':
     data['inverse_mse'] = data.progress_apply(lambda row: 1 / calc_mse(row['coverage'], row['BAD']).mean(), axis=1)
     df = df.merge(data, on=['BAD', 'coverage'])
     assert initial_df_len == len(df.index)
+    df.to_csv(args.output, index=False, sep='\t')
     
