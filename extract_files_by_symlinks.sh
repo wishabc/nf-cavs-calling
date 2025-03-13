750e3e6b989b74db450f493a1b257645e1655180
wishabc <aswq22013@gmail.com>
2025-03-14 02:10:43 +0800

    fixed extract files by symlink

--- extract_files_by_symlinks.sh
+++ extract_files_by_symlinks.sh
@@ -1,26 +1,53 @@
 #!/bin/bash
-# Usage: extract symlink <target-dir> <-f | -n>
-# -f moves symlink targets
-# -n perform a 'dry run', printing all the commands to be performed
+# Usage: extract_symlink <target-dir> <-f | -n>
+# -f moves symlink targets safely
+# -n performs a 'dry run', printing all the commands to be performed
 
-# TODO: add graceful error handling
+set -e  # Exit on error
 
 function extract_symlink () {
-	if ! [ -d "$1" ]; then
-        if [ -h "$1" ]; then
-            mv $( realpath "$1" ) $1
+    local link="$1"
+    
+    if [ -h "$link" ]; then
+        local target
+        target=$(realpath "$link")
+
+        if [ -e "$target" ]; then
+            local dir
+            dir=$(dirname "$link")
+            local tmpfile
+            tmpfile=$(mktemp --tmpdir="$dir" "symlink_extract_XXXXXX")
+
+            cp -r "$target" "$tmpfile" && rm "$link" && mv "$tmpfile" "$link"
+        else
+            echo "Error: Target '$target' does not exist" >&2
         fi
-	fi
+    fi
 }
 export -f extract_symlink
 
-case $2 in
+if [ ! -d "$1" ]; then
+    echo "Error: Target directory '$1' does not exist or is not a directory" >&2
+    exit 1
+fi
+
+case "$2" in
     "-n")
-        find $1 -type l -exec bash -c 'a="$@"; b=$( realpath $a ); if [[ "$b" != "$a" ]]; then echo mv $b $a; fi' bash {} \;
+        find "$1" -type l -print0 | while IFS= read -r -d '' link; do
+            target=$(realpath "$link")
+            if [[ -e "$target" ]]; then
+                dir=$(dirname "$link")
+                echo "cp -r \"$target\" \"$dir/tmpfile\" && rm \"$link\" && mv \"$dir/tmpfile\" \"$link\""
+            else
+                echo "Error: Target '$target' does not exist" >&2
+            fi
+        done
         ;;
     "-f")
-        find $1 -type l -exec bash -c 'extract_symlink "$@"' bash {} \;
+        find "$1" -type l -print0 | xargs -0 -I {} bash -c 'extract_symlink "$@"' _ {}
         ;;
-    "*") 
-        echo "Neither -f nor -n are provided"
+    *)
+        echo "Error: Neither -f nor -n are provided" >&2
+        exit 1
+        ;;
 esac
